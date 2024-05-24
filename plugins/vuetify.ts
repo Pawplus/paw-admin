bf9f82622df2a6ae5a3cc939af2a1f9fea4d0ae4
trian damai <triannurizkillah@gmail.com>
2024-05-24 21:00:19 +0700

    feat: initiate

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,7 +4,7 @@ import "@mdi/font/css/materialdesignicons.css";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import PerfectScrollbar from 'vue3-perfect-scrollbar';
-import VueApexCharts from 'vue3-apexcharts';
+// import VueApexCharts from 'vue3-apexcharts';
 import VueTablerIcons from 'vue-tabler-icons';
 import '@/scss/style.scss';
 import {
@@ -29,7 +29,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   // @ts-ignore
   nuxtApp.vueApp.use(PerfectScrollbar);
   // @ts-ignore
-  nuxtApp.vueApp.use(VueApexCharts);
+  // nuxtApp.vueApp.use(VueApexCharts);
   // @ts-ignore
   nuxtApp.vueApp.use(VueTablerIcons);
 });
