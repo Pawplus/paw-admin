136551215ec9695ea580a03da7e5d3c2c98bac1b
maulanafikriash <maulanx835@gmail.com>
2024-05-24 22:06:42 +0700

    fix: indentasi

--- pages/admin-dashboard.vue
+++ pages/admin-dashboard.vue
@@ -4,41 +4,34 @@ import SalesOverview from '~/components/dashboard/SalesOverview.vue';
 import YearlyBreakup from '~/components/dashboard/YearlyBreakup.vue';
 </script>
 <template>
-    <v-app>
-      <v-main>
-        <v-container>
-          <v-row no-gutters>
-            <v-col cols="12" md="3">
-              <!-- <DashboardSideBar :role="'superadmin'" /> -->
-              <DashboardSideBar :role="'adminklinik'" />
-            </v-col>
-            <v-col cols="12" md="13">
-              <!-- <DashboardCards :role="'superadmin'" /> -->
-              <DashboardCards :role="'adminklinik'" />
-            </v-col>
-            <v-col cols="12" md="8">
-              <SalesOverview/>
-            </v-col>
-            <v-col cols="12" md="4">
-                <div class="mb-6">
-                    <YearlyBreakup/>
-                </div>
-                <div>
-                    <ConsulClinic/>
-                </div>
-            </v-col>
-            <v-col cols="12" md="15">
-                <DashboardPaymentVerification/>
-              </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </template>
-    
-  <style scoped>
-  .pa-0 {
-    padding: 0;
-  }
-  </style>
-  
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row no-gutters>
+          <v-col cols="12" md="3">
+            <!-- <DashboardSideBar :role="'superadmin'" /> -->
+            <DashboardSideBar :role="'adminklinik'" />
+          </v-col>
+          <v-col cols="12" md="13">
+            <!-- <DashboardCards :role="'superadmin'" /> -->
+            <DashboardCards :role="'adminklinik'" />
+          </v-col>
+          <v-col cols="12" md="8">
+            <SalesOverview/>
+          </v-col>
+          <v-col cols="12" md="4">
+            <div class="mb-6">
+              <YearlyBreakup />
+            </div>
+            <div>
+              <ConsulClinic />
+            </div>
+          </v-col>
+          <v-col cols="12" md="15">
+            <DashboardPaymentVerification />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
