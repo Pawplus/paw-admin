aa4a17c228d9fb7d6379f61ea6d034fd08156769
maulanafikriash <maulanx835@gmail.com>
2024-05-24 23:23:32 +0700

    fix: component dsh

--- components/dashboard/YearlyBreakup.vue
+++ components/dashboard/YearlyBreakup.vue
@@ -4,7 +4,42 @@ import { useTheme } from "vuetify";
 const theme = useTheme();
 const primary = theme.current.value.colors.primary;
 const lightprimary = theme.current.value.colors.lightprimary;
+const chartOptions = computed(() => {
+  return {
+    labels: ["series-1", "series-2", "series-3"],
+    chart: {
+      type: "donut",
+      fontFamily: `inherit`,
+      foreColor: "#a1aab2",
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: [primary, lightprimary, "#F9F9FD"],
+    plotOptions: {
+      pie: {
+        startAngle: 0,
+        endAngle: 360,
+        donut: {
+          size: "75%",
+          background: "transparent",
+        },
+      },
+    },
+    stroke: {
+      show: false,
+    },
 
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    tooltip: { theme: "light", fillSeriesColor: false },
+  };
+});
+const Chart = [38, 40, 25];
 </script>
 <template>
   <v-card elevation="10" class="withbg">
@@ -25,21 +60,11 @@ const lightprimary = theme.current.value.colors.lightprimary;
             </div>
             <div class="d-flex align-center mt-sm-10 mt-8">
               <h6 class="text-subtitle-1 text-muted">
-                <v-icon
-                  icon="mdi mdi-checkbox-blank-circle"
-                  class="mr-1"
-                  size="10"
-                  color="primary"
-                ></v-icon>
+                <v-icon icon="mdi mdi-checkbox-blank-circle" class="mr-1" size="10" color="primary"></v-icon>
                 2022
               </h6>
               <h6 class="text-subtitle-1 text-muted pl-5">
-                <v-icon
-                  icon="mdi mdi-checkbox-blank-circle"
-                  class="mr-1"
-                  size="10"
-                  color="lightprimary"
-                ></v-icon>
+                <v-icon icon="mdi mdi-checkbox-blank-circle" class="mr-1" size="10" color="lightprimary"></v-icon>
                 2023
               </h6>
             </div>
@@ -47,7 +72,8 @@ const lightprimary = theme.current.value.colors.lightprimary;
         </v-col>
         <v-col cols="5" sm="5" class="pl-lg-0">
           <div class="d-flex align-center flex-shrink-0">
-
+            <apexchart class="pt-6" type="donut" height="145" :options="chartOptions" :series="Chart">
+            </apexchart>
           </div>
         </v-col>
       </v-row>
