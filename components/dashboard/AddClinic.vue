b05161eb772f7da0e4aa6aa9047ad28cb0f46685
maulanafikriash <maulanx835@gmail.com>
2024-06-13 11:29:51 +0700

    fix: add element popup and types

--- components/dashboard/AddClinic.vue
+++ components/dashboard/AddClinic.vue
@@ -1,37 +1,25 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, computed } from 'vue';
+import type { AddClinic } from '~/types/dashboard';
+import { useRouter } from 'vue-router';
+import PopupModalAddClinic from './PopupModalAddClinic.vue';
 
-const router = useRouter()
-const location = ref('')
+const router = useRouter();
+const location = ref('');
 
 const googleMapsEmbedUrl = computed(() => {
     return "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2939.762763812713!2d107.04607566781714!3d-6.205292379297995!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698900248cc3f1%3A0x9b3430faf772b550!2sDarmawangsa%20Residence%20Cluster%20Sriwijaya!5e0!3m2!1sid!2sid!4v1717928813108!5m2!1sid!2sid";
-})
+});
 
 const goBack = () => {
-  router.back()
-}
+    router.back();
+};
 
 const addOperationalHour = () => {
-    clinic.value.operationalHours.push({ day: '', open: '', close: '' })
-}
-interface Clinic {
-    name: string,
-    email: string,
-    address: {
-        street: string,
-        rt: string,
-        rw: string,
-        subdistrict: string,
-        district: string,
-        city: string,
-        postalCode: string
-    },
-    operationalHours: Array<{ day: string, open: string, close: string }>
-}
+    clinic.value.operationalHours.push({ day: '', open: '', close: '' });
+};
 
-const clinic = ref<Clinic>({
+const clinic = ref<AddClinic>({
     name: '',
     email: '',
     address: {
@@ -46,19 +34,19 @@ const clinic = ref<Clinic>({
     operationalHours: [
         { day: 'Senin', open: '09:00', close: '21:00' },
         { day: 'Selasa', open: '09:00', close: '21:00' }
-    
     ]
-})
+});
 
-const days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat']
+const days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
 
-const subdistricts = ref(['Bojong Gede', 'Bahagia']) // replace with real data
-const districts = ref(['Cilandak', 'Mampang Prapatan']) // replace with real data
-const cities = ref(['City 1', 'City 2']) // replace with real data
+const subdistricts = ref(['Bojong Gede', 'Bahagia']); // replace with real data
+const districts = ref(['Cilandak', 'Mampang Prapatan']); // replace with real data
+const cities = ref(['City 1', 'City 2']); // replace with real data
 
 const files = ref<string[]>([]);
 const inputFile = ref<File | null>(null);
 const isUploading = ref(false);
+const isPopupOpen = ref(false);
 
 const handleFileUpload = () => {
     if (inputFile.value) {
@@ -78,29 +66,25 @@ const removeFile = (index: number) => {
 const submit = () => {
     if (files.value.length >= 3 && !isUploading.value) {
         isUploading.value = true;
-        // Logic to upload files to server
         console.log('Files uploaded:', files.value);
-        // Reset files after upload
-        files.value = [];
         isUploading.value = false;
+        isPopupOpen.value = true;
     }
 };
-
 </script>
-
 
 <template>
     <v-container>
         <v-row class="mb-3" align="center">
             <v-col cols="auto">
-              <v-btn icon @click="goBack">
-                <v-icon>mdi-arrow-left</v-icon>
-              </v-btn>
+                <v-btn icon @click="goBack">
+                    <v-icon>mdi-arrow-left</v-icon>
+                </v-btn>
             </v-col>
             <v-col>
-              <v-card-title>Tambah Klinik</v-card-title>
+                <v-card-title>Tambah Klinik</v-card-title>
             </v-col>
-          </v-row>
+        </v-row>
         <v-card>
             <v-card-text>
                 <v-form>
@@ -122,21 +106,17 @@ const submit = () => {
                         </v-col>
                     </v-row>
 
-                    <v-select label="Kelurahan" :items="subdistricts"
-                        v-model="clinic.address.subdistrict" />
-                    <v-select label="Kecamatan" :items="districts"
-                        v-model="clinic.address.district" />
+                    <v-select label="Kelurahan" :items="subdistricts" v-model="clinic.address.subdistrict" />
+                    <v-select label="Kecamatan" :items="districts" v-model="clinic.address.district" />
 
                     <v-row>
                         <v-col cols="8">
-                            <v-select label="Kota" :items="cities"
-                                v-model="clinic.address.city" />
+                            <v-select label="Kota" :items="cities" v-model="clinic.address.city" />
                         </v-col>
                         <v-col cols="4">
                             <v-text-field label="Kode pos" v-model="clinic.address.postalCode" />
                         </v-col>
                     </v-row>
-
                 </v-form>
             </v-card-text>
         </v-card>
@@ -195,9 +175,9 @@ const submit = () => {
                 Buat Klinik
             </v-btn>
         </v-card>
+        <PopupModalAddClinic v-model:isDialogOpen="isPopupOpen" :clinic="clinic" :files="files" />
     </v-container>
 </template>
-
 
 <style scoped>
 .v-card {
--- types/dashboard/index.ts
+++ types/dashboard/index.ts
@@ -1,75 +1,96 @@
 /*Recent Transaction*/
 type recentTrans = {
-    title: string;
-    subtitle: string;
-    textcolor: string;
-    boldtext: boolean;
-    line: boolean;
-    link: string;
-    url: string;
+  title: string;
+  subtitle: string;
+  textcolor: string;
+  boldtext: boolean;
+  line: boolean;
+  link: string;
+  url: string;
 };
 
 /*product performance*/
 type productPerformanceType = {
-    id: number;
-    name: string;
-    post: string;
-    pname: string;
-    status: string;
-    statuscolor: string;
-    budget: string;
+  id: number;
+  name: string;
+  post: string;
+  pname: string;
+  status: string;
+  statuscolor: string;
+  budget: string;
 };
 
 /*Products card types*/
 type productsCards = {
-    title: string;
-    link: string;
-    photo: string;
-    salesPrice: number;
-    price: number;
-    rating: number;
+  title: string;
+  link: string;
+  photo: string;
+  salesPrice: number;
+  price: number;
+  rating: number;
 };
 
 // payment verification types
 type Payment = {
-    no: number
-    orderId: string
-    orderDate: string
-    service: string
-    userEmail: string
-    doctor: string
-    paymentAmount: number
-    uniqueCode: string
-    paymentMethod: string
-    action: string
-  }
+  no: number;
+  orderId: string;
+  orderDate: string;
+  service: string;
+  userEmail: string;
+  doctor: string;
+  paymentAmount: number;
+  uniqueCode: string;
+  paymentMethod: string;
+  action: string;
+};
 
 //   Detail Consultation types
 type Consultation = {
-    id: string;
-    orderDate: string;
-    orderTime: string;
-    email: string;
+  id: string;
+  orderDate: string;
+  orderTime: string;
+  email: string;
+  service: string;
+  doctor: string;
+  paymentAmount: string;
+  uniqueCode: string;
+  paymentMethod: string;
+  pet: {
+    name: string;
+    age: string;
+    breed: string;
+    gender: string;
+    notes: string;
+  };
+  history: Array<{
+    date: string;
     service: string;
-    doctor: string;
-    paymentAmount: string;
-    uniqueCode: string;
-    paymentMethod: string;
-    pet: {
-        name: string;
-        age: string;
-        breed: string;
-        gender: string;
-        notes: string;
-    };
-    history: Array<{
-        date: string;
-        service: string;
-        notes: string;
-        chatLink: string;
-    }>;
-}
+    notes: string;
+    chatLink: string;
+  }>;
+};
 
+// Add Clinic types
+type AddClinic = {
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    rt: string;
+    rw: string;
+    subdistrict: string;
+    district: string;
+    city: string;
+    postalCode: string;
+  };
+  operationalHours: Array<{ day: string; open: string; close: string }>;
+};
 
-
-export type { recentTrans, productPerformanceType, productsCards, Payment, Consultation }
+export type {
+  recentTrans,
+  productPerformanceType,
+  productsCards,
+  Payment,
+  Consultation,
+  AddClinic
+};
